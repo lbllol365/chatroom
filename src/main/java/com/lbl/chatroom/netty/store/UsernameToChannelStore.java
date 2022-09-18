c82829825a3fefd39b979e0079827d480bb1363c
lbllol365 <1217584374@qq.com>
2022-09-18 18:04:22 +0800

    登录保存用户Handler

---
+++ src/main/java/com/lbl/chatroom/netty/store/UsernameToChannelStore.java
@@ -0,0 +1,18 @@
+package com.lbl.chatroom.netty.store;
+
+import io.netty.channel.Channel;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+
+public class UsernameToChannelStore {
+    private static final ConcurrentHashMap<String, Channel> USERNAME_TO_CHANNEL_MAP = new ConcurrentHashMap<>();
+
+    public static Channel getChannelByUsername(String username) {
+        return USERNAME_TO_CHANNEL_MAP.get(username);
+    }
+
+    public static void storeChannel(String username, Channel channel) {
+        USERNAME_TO_CHANNEL_MAP.put(username, channel);
+    }
+}
